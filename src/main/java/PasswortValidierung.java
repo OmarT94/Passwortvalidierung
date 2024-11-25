0dfbe13faab630fe17117e3aa2a3546ac4d99506
OmarT94 <93581484+OmarT94@users.noreply.github.com>
2024-11-25 13:04:48 +0100

    Beendigung der Tasks

---
+++ src/main/java/PasswortValidierung.java
@@ -0,0 +1,40 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class PasswortValidierung {
+
+    public static String IstPasswortGrosserAls_AchtZeichen(String passwort) {
+
+//        return passwort != null && passwort.length()>=8;
+        if (passwort != null && passwort.length() >= 8) {
+            return passwort;
+
+        }return null;
+    }
+
+    public static String Passwortenthaelt_Ziffern(String passwort) {
+        if (passwort != null && passwort.matches(".*\\d.*")) {
+            return passwort;
+        }return null;
+    }
+
+    public static String Passwort_Enthaelt_GrossUndKleinBuchstaben(String passwort) {
+        if (passwort != null && passwort.matches(".*[a-z].*") && passwort.matches(".*[A-Z].*")) {
+            return passwort;
+        }return null;
+    }
+
+    public static boolean IstPasswort_Haufig_Verwendet(String passwort) {
+
+        String [] haeufigeVerwendetesPasswort={"Aa345678","12345678","qwertzui","qwerty","passwort","abcdefghi",
+                "passwort1","passwort2","passwort3","passwort4"};
+        for (String commonPasswort: haeufigeVerwendetesPasswort) {
+            if (commonPasswort.equals(passwort)) {
+                return true;
+            }
+        }return false;
+
+    }
+
+
+
+}
---
+++ src/test/java/Test_PasswortValidierung.java
@@ -0,0 +1,48 @@
+
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.management.StringValueExp;
+
+public class Test_PasswortValidierung {
+
+
+    @Test
+    void Check_If_Passwort_IstGrosserAls_Acht_Zeichen() {
+
+        String Acceptedpasswort ="qwertamr" ;
+        String actulPasswort=PasswortValidierung.IstPasswortGrosserAls_AchtZeichen(Acceptedpasswort);
+        Assertions.assertEquals(Acceptedpasswort,actulPasswort);
+
+
+    }
+
+    @Test
+
+    void Chech_If_Passwort_enthaelt_Ziffern(){
+        String Acceptedpasswort ="Omartamr1" ;
+        String actulPasswort= PasswortValidierung.Passwortenthaelt_Ziffern(Acceptedpasswort);
+        Assertions.assertEquals(Acceptedpasswort,actulPasswort);
+
+    }
+
+
+    @Test
+    void Check_If_PasswortEnthealt_GrossUndKlein_Buchstaben(){
+
+        String Acceptedpasswort ="OMAR TAMr" ;
+        String actulPasswort= PasswortValidierung.Passwort_Enthaelt_GrossUndKleinBuchstaben(Acceptedpasswort);
+        Assertions.assertEquals(Acceptedpasswort,actulPasswort);
+    }
+
+    @Test
+    void Check_If_Passwoet_HaeufigVerwendet(){
+          String haeufigVerwendet ="Aa345678" ;
+//        String nichHaeufigpasswort="Omar Tamr";
+          Assertions.assertTrue(PasswortValidierung.IstPasswort_Haufig_Verwendet(haeufigVerwendet));
+//        Assertions.assertTrue(PasswortValidierung.IstPasswort_Haufig_Verwendet("12345678tt"));
+    }
+
+}
